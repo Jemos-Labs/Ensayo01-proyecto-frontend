3a237e5e6baf35f981fefca1c55c97962d697338
jemosdev <jerson.mosquera.p@gmail.com>
2024-12-01 22:01:51 -0500

    feat: añadir configuracion inicial del proyecto

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+const App = () => {
+    return (
+        <div>
+            <h1>Hello, Vite + React!</h1>
+            <h1>Ensayo-proyecto01-frontend</h1>
+        </div>
+    );
+};
+
+ReactDOM.render(<App />, document.getElementById('app'));
